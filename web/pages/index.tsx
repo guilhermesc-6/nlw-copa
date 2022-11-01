45de0e63afe4cc9306a9555fe1fc695582f7de07
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2022-11-01 15:18:38 -0300

    nlw-aula1.

---
+++ web/pages/index.tsx
@@ -0,0 +1,15 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count}</h1>;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:2222/pools/count");
+  const data = await response.json();
+
+  console.log(data);
+  return { props: { count: data.count } };
+};
